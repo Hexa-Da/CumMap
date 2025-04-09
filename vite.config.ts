97e75be273596d87e02b2df5a699495f3471d15d
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-04-09 10:48:25 +0200

    Corrigé les problèmes de chemins pour le déploiement

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    sourcemap: true
   }
 })
