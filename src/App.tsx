361d41a4bc8a2216e514663f94333649a10890ba
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-04-12 02:05:16 +0200

    Amélioration du style du bouton plein écran : position et design

--- src/App.tsx
+++ src/App.tsx
@@ -2034,16 +2034,17 @@ function App() {
       <button 
         className={`fullscreen-button ${isFullscreen ? 'active' : ''}`}
         onClick={toggleFullscreen}
-        title={isFullscreen ? "Quitter le mode plein écran" : "Activer le mode plein écran"}
+        title={isFullscreen ? "Quitter le mode plein écran" : "Mode plein écran"}
       >
-        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          {isFullscreen ? (
+        {isFullscreen ? (
+          <svg viewBox="0 0 24 24">
             <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
-          ) : (
+          </svg>
+        ) : (
+          <svg viewBox="0 0 24 24">
             <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
-          )}
-        </svg>
-        {isFullscreen ? "Plein écran" : "Plein écran"}
+          </svg>
+        )}
       </button>
     </div>
   );
