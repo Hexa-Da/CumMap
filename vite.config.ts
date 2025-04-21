5d7bab2b18f23030c109c07b860d67c110bdcc0c
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-04-21 16:13:21 +0200

    Fix manifest.json handling

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
@@ -14,5 +14,6 @@ export default defineConfig({
         }
       }
     }
-  }
+  },
+  publicDir: 'public'
 })
