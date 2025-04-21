841d5b817b96e3f8b1da0bae1b6e403dcf8bc8ee
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-04-21 16:08:20 +0200

    Update base path and simplify configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     rollupOptions: {
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
           'leaflet-vendor': ['leaflet', 'react-leaflet']
-        },
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+        }
       }
-    },
-    assetsInlineLimit: 0
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript'
     }
   }
 })
