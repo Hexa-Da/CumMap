9ce1a5ca371b7062003979df018efd0df8bd1a02
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-06-16 17:11:50 +0200

    Remove Firebase API keys from source code

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFB_-a5O4KD1V0MSa4HYpsEMekpBTL044",
-  authDomain: "cummap-7afee.firebaseapp.com",
-  databaseURL: "https://cummap-7afee-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cummap-7afee",
-  storageBucket: "cummap-7afee.firebasestorage.app",
-  messagingSenderId: "402641775282",
-  appId: "1:402641775282:web:585cabb0a67ae4475937ab"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const database = getDatabase(app);
