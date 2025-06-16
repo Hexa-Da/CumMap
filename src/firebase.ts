34a29852dd39badcc06480c9d75366346466fa5e
Paul-Antoine Paris <135511378+Hexa-Da@users.noreply.github.com>
2025-06-16 16:59:27 +0200

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFB_-a5O4KD1V0MSa4HYpsEMekpBTL044",
-  authDomain: "cummap-7afee.firebaseapp.com",
-  databaseURL: "https://cummap-7afee-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "cummap-7afee",
-  storageBucket: "cummap-7afee.firebasestorage.app",
-  messagingSenderId: "402641775282",
-  appId: "1:402641775282:web:585cabb0a67ae4475937ab"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
